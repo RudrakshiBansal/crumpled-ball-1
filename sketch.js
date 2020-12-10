053603bd4740778a0d05019078a68313ec573a59
RudrakshiBansal <73390426+RudrakshiBansal@users.noreply.github.com>
2020-12-10 12:21:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var paper,ground,side0,side1,side2;
+
+function preload(){
+   
+}
+
+function setup() {
+	createCanvas(1500, 400);
+ 
+   
+	engine = Engine.create();
+	world = engine.world;
+
+	paper = new Paper(350, 200, 20);
+
+	ground = new Ground(750, 380, 1500, 20);
+ 
+	side0 = new Rectangle(1250, 320, 20, 100);
+	side1 = new Rectangle(1335, 360, 150, 20);
+	side2 = new Rectangle(1420, 320, 20, 100);
+
+
+	Engine.run(engine);
+ 
+}
+
+
+function draw() {
+
+ rectMode(CENTER);
+ background(0);
+
+ Engine.update(engine);
+
+ paper.display();
+
+ ground.display();
+
+ side0.display();
+ side1.display();
+ side2.display();
+
+ drawSprites();
+
+}
+
+function keyPressed(){
+   if(keyCode === UP_ARROW)  {
+	  Matter.Body.applyForce(paper.body, paper.body.position, 
+		  {x:85, y: -85})
+   }
+
+ }
+
